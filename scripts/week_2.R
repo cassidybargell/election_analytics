722f254a83533902eb742059765e33ad0c512cc3
cassidybargell <cassidybargell@college.harvard.edu>
2020-09-19 12:08:42 -0600

    update popvote with new incumbency data

--- scripts/week_2.R
+++ scripts/week_2.R
@@ -20,3 +20,13 @@ local_econ <- read_csv("data/local.csv")
 # Create states map
 states_map <- usmap::us_map()
 unique(states_map$abbr)
+
+# Join econ and popvote, keep all quarters including 2020
+dat <- econ %>% 
+  filter(year >= 1948) %>%
+  slice(1:290) %>% # want to keep all quarters not just 1 and/or 2
+  full_join(popvote %>% filter(incumbent_party == TRUE) %>%
+               select(year, winner, pv2p)) %>%
+  mutate(winner = ifelse(year == 2020, "?", winner)) %>% # easy way to keep 2020 
+  filter(! is.na(winner)) %>%
+  mutate(winner = ifelse(year == 2020, NA, winner))
